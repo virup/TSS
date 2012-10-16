f47bde803b47205c4a47aa5f8753e61176481fe2
virup_mac <v.kanjilal@gmail.com>
2012-10-16 08:24:40 +0200

    Bug fix test

--- stressTestSegment.cpp
+++ stressTestSegment.cpp
@@ -282,13 +282,13 @@ void write_to_blob(int number)
         cout<<"Done 3 "<<endl;
         Locator lRightPt = l.insert(1);
         cout<<"Done 4 "<<endl;
-        Locator lLPTx = lLeftPt.insert(0);
+        Locator lLPTx = p.insert(lLeftPt,0);
         cout<<"Done 5 "<<endl;
-        Locator lLPTy = lLeftPt.insert(1);
+        Locator lLPTy = p.insert(lLeftPt, 0);
         cout<<"Done 6 "<<endl;
-        Locator lRPTx = lRightPt.insert(0);
+        Locator lRPTx = p.insert(lRightPt,0);
         cout<<"Done 7 "<<endl;
-        Locator lRPTy = lRightPt.insert(1);
+        Locator lRPTy = p.insert(lRightPt,0);
         cout<<"Done 8 "<<endl;
         double x1 = fRand(0,MAX);
         double y1 = fRand(0,MAX);
